61721f7e550c2c996f070bf214672321dbcc6f0a
Renan Fernandes <renzinestuods@gmail.com>
2022-11-30 18:56:46 -0300

    :sparkles: feat: Cria o migrateon da tabela blog_posts

---
+++ src/migrations/20221130203051-create-blog-posts.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primareKey: true,
+        type: Sequelize.STRING,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'user_id',
+        references: {
+          model: 'users',
+          key: 'id',
+        }
+      },
+      published: {
+        allowNull: false,
+      },
+      updated: {
+        allowNull: false,
+      }
+    })
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
