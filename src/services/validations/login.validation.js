2df138e37374480561b934ba85978a89c9487e85
Renan Fernandes <renzinestuods@gmail.com>
2022-12-01 20:54:28 -0300

    :boom: fix: Adiciona um valor padrão para a chave error

--- src/services/validations/login.validation.js
+++ src/services/validations/login.validation.js
@@ -1,7 +1,8 @@
 const { checkLogin } = require('./shema');
 
 const validateLogin = (fields) => {
-  const { error: [{ message }] } = checkLogin.validate(fields);
+  const { error = { details: [{}] } } = checkLogin.validate(fields);
+  const { details: [{ message }] } = error;
   if (message) return { type: 400, message };
   return { type: null, message: '' };
 };
