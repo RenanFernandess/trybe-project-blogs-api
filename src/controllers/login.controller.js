0699a752cb73e9c6c6a8d600de052256ace9fa15
Renan Fernandes <renzinestuods@gmail.com>
2022-12-02 16:40:15 -0300

    :sparkles: feat: Desestrutura o req.body para pegar somente as informações necessarias

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,7 +1,7 @@
 const loginService = require('../services/login.service');
 
-const login = async ({ body }, res) => {
-  const { type, message } = await loginService.login(body);
+const login = async ({ body: { email, password } }, res) => {
+  const { type, message } = await loginService.login({ email, password });
   if (type) return res.status(type).json({ message });
 
   return res.status(200).json({ token: message });
