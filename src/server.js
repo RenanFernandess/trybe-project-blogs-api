3939148c7ead86c6fbb7fdc3c1b2542bc7f59875
Renan Fernandes <renzinestuods@gmail.com>
2022-12-02 21:10:35 -0300

    :sparkles: feat: Adiciona a rota '/user'

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
 const { loginRoute } = require('./routes/login.route');
+const { userRoute } = require('./routes/user.route');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -11,5 +12,6 @@ app.get('/', (_request, response) => {
 });
 
 app.use('/login', loginRoute);
+app.use('/user', userRoute);
 
 app.listen(port, () => console.log('ouvindo porta', port));
