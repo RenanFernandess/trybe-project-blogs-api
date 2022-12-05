a119ad9691385f4d36cdc573312259a81c851a18
Renan Fernandes <renzinestuods@gmail.com>
2022-12-05 20:02:10 -0300

    :sparkles: feat: Trata o error utilizando try catch e retorna a mensagem 'Expired or invalid token' caso o jwt retorne algum error

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,11 +1,15 @@
 const { checkToken } = require('../auth/validateJwt');
 
 const validateToken = async (req, res, next) => {
-  const { headers: { authorization } } = req;
-  if (!authorization) return res.status(401).json({ message: 'Token not found' });
-  const { type, message } = await checkToken(authorization);
-  if (type) return res.status(type).json({ message });
-  return next();
+    const { headers: { authorization } } = req;
+    if (!authorization) return res.status(401).json({ message: 'Token not found' });
+  try {
+    const { type, message } = await checkToken(authorization);
+    if (type) return res.status(type).json({ message });
+    return next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
 };
 
 module.exports = { validateToken };
