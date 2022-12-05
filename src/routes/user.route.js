968c4195a91c3bed7ac0f7db980f4d37cd66a40a
Renan Fernandes <renzinestuods@gmail.com>
2022-12-05 20:02:54 -0300

    :sparkles: feat: Cria a rota get '/user'

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/user.controller');
+const { validateToken } = require('../middlewares/validateToken');
 
 const userRoute = express.Router();
 
 userRoute.post('/', userController.register);
+
+userRoute.get('/', validateToken, userController.getAllUsers);
 
 module.exports = { userRoute };
