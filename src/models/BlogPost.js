90b86fc363d87b956d4dfd235cd92b480c4fd8fe
Renan Fernandes <renzinestuods@gmail.com>
2022-12-06 18:13:36 -0300

    :sparkles: feat: Cria o model da tabela 'blog_posts'

---
+++ src/models/BlogPost.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  },
+  {
+    underscored: true,
+    timestamps: false,
+    tableName: 'blog_posts',
+  })
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'users'
+    })
+  }
+
+  return BlogPost;
+};
