02da8d643889ed25e72097876ff2f0a7d51da598
Renan Fernandes <renzinestuods@gmail.com>
2022-12-09 15:57:19 -0300

    Remove console.log

--- src/services/validations/post.validation.js
+++ src/services/validations/post.validation.js
@@ -3,7 +3,6 @@ const { checkPost } = require('./shema');
 const validatePost = (fields) => {
   const { error = { details: [{}] } } = checkPost.validate(fields);
   const { details: [{ message }] } = error;
-  console.log(error.details);
   if (message) return { type: 400, message };
   return { type: null, message: '' };
 };
