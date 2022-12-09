76558026b64106e980f1de0add42bfe6eee7a8e0
Renan Fernandes <renzinestuods@gmail.com>
2022-12-09 11:43:20 -0300

    :sparkles: feat: Adiciona a chave user na requisição

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -6,6 +6,7 @@ const validateToken = async (req, res, next) => {
   try {
     const { type, message } = await checkToken(authorization);
     if (type) return res.status(type).json({ message });
+    req.user = message;
     return next();
   } catch (error) {
     return res.status(401).json({ message: 'Expired or invalid token' });
