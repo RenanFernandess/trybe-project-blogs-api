86dcceb5f27271c7799bde5523e23cdb2e10e424
Renan Fernandes <renzinestuods@gmail.com>
2022-12-09 16:10:58 -0300

    :sparkles: feat: Adiciona a rota '/post'

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const app = require('./app');
 const { categoriesRoute } = require('./routes/categories.rote');
 const { loginRoute } = require('./routes/login.route');
 const { userRoute } = require('./routes/user.route');
+const { postRoute } = require('./routes/post.route');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -15,5 +16,6 @@ app.get('/', (_request, response) => {
 app.use('/login', loginRoute);
 app.use('/user', userRoute);
 app.use('/categories', categoriesRoute);
+app.use('/post', postRoute);
 
 app.listen(port, () => console.log('ouvindo porta', port));
