add9d58948cf16cb2f4120a048571e031be9da9c
Renan Fernandes <renzinestuods@gmail.com>
2022-12-09 15:59:26 -0300

    :sparkles: feat: Verifica se as categorias passadas pelo cliente são existentes

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,20 +1,28 @@
-const { BlogPost, PostCategory, sequelize } = require('../models');
+const { BlogPost, PostCategory, Category, sequelize } = require('../models');
 const { validatePost } = require('./validations/post.validation');
 
-const setPost = async ({ categoryIds, ...post }) => {
-  const fieldError = validatePost({ categoryIds, ...post });
+const setPostAndCategory = async ({ categoryIds, ...post }) => {
+  const result = await sequelize.transaction(async (t) => {
+    const date = new Date();
+    const { dataValues, dataValues: { id: postId } } = await BlogPost.create({
+      ...post, published: date, updated: date,
+    }, { transaction: t });
+    const postCategories = categoryIds.map((categoryId) => ({ categoryId, postId }));
+    await PostCategory.bulkCreate(postCategories, { transaction: t });
+    return dataValues;
+  });
+  return result;
+};
+
+const setPost = async (fields) => {
+  const { categoryIds } = fields;
+  const fieldError = validatePost(fields);
   if (fieldError.type) return fieldError;
-  const date = new Date();
   try {
-    const result = await sequelize.transaction(async (t) => {
-      const { dataValues, dataValues: { id: postId } } = await BlogPost.create({
-        ...post, published: date, updated: date,
-      }, { transaction: t });
-      const postCategories = categoryIds.map((categoryId) => ({ categoryId, postId }));
-      await PostCategory.bulkCreate(postCategories, { transaction: t });
-      return dataValues;
-    });
-    return { type: null, message: result };
+    const checkCategories = (await Promise.all(categoryIds.map((id) => Category.findByPk(id))))
+    .every((item) => item);
+    if (!checkCategories) return { type: 400, message: 'one or more "categoryIds" not found' };
+    return { type: null, message: await setPostAndCategory(fields) };
   } catch (error) { return error; }
 };
 
