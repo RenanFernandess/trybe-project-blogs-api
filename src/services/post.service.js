5df48606084cbcac5f69e22d6e13fd7e2ad95a40
Renan Fernandes <renzinestuods@gmail.com>
2022-12-09 12:45:36 -0300

    :sparkles: feat: Adiciona a validação dos campos utilizando a função validatePost

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,19 +1,21 @@
 const { BlogPost, PostCategory, sequelize } = require('../models');
+const { validatePost } = require('./validations/post.validation');
 
 const setPost = async ({ categoryIds, ...post }) => {
+  const fieldError = validatePost({ categoryIds, ...post });
+  if (fieldError.type) return fieldError;
+  const date = new Date();
   try {
     const result = await sequelize.transaction(async (t) => {
-      const { dataValues, dataValues: { id: postId } } = await BlogPost.Create({
-        ...post, published: 'NOW()', updated: 'NOW()',
+      const { dataValues, dataValues: { id: postId } } = await BlogPost.create({
+        ...post, published: date, updated: date,
       }, { transaction: t });
       const postCategories = categoryIds.map((categoryId) => ({ categoryId, postId }));
       await PostCategory.bulkCreate(postCategories, { transaction: t });
       return dataValues;
     });
     return { type: null, message: result };
-  } catch (error) {
-    return { type: 500, message: '' };
-  }
+  } catch (error) { return error; }
 };
 
 module.exports = { setPost };
