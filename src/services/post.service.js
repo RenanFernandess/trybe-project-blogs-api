d13d27a7eff19509f4cd76b6a3e6e06c441509ee
Renan Fernandes <renzinestuods@gmail.com>
2022-12-09 18:10:25 -0300

    :sparkles: feat: Cria a função getAllPost, que solicita ao model todos os postscom as categorias e a informação do usuario

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,5 +1,7 @@
-const { BlogPost, PostCategory, Category, sequelize } = require('../models');
+const { BlogPost, PostCategory, User, Category, sequelize } = require('../models');
 const { validatePost } = require('./validations/post.validation');
+
+const INTERNAL_ERROR = { type: 500, message: 'Internal server error' };
 
 const setPostAndCategory = async ({ categoryIds, ...post }) => {
   const result = await sequelize.transaction(async (t) => {
@@ -23,7 +25,30 @@ const setPost = async (fields) => {
     .every((item) => item);
     if (!checkCategories) return { type: 400, message: 'one or more "categoryIds" not found' };
     return { type: null, message: await setPostAndCategory(fields) };
-  } catch (error) { return error; }
+  } catch (error) { return INTERNAL_ERROR; }
 };
 
-module.exports = { setPost };
+const getAllPost = async () => {
+  try {
+    const result = await BlogPost.findAll({
+      include: [
+        {
+          model: Category,
+          as: 'categories',
+          through: { attributes: [] },
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: { exclude: ['password'] },
+        },
+      ],
+    });
+    return { type: null, message: result };
+  } catch (_e) { return INTERNAL_ERROR; }
+};
+
+module.exports = {
+  setPost,
+  getAllPost,
+};
