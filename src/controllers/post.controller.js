373e7766e88ac7e789854c4d91582bbf1f14d44c
Renan Fernandes <renzinestuods@gmail.com>
2023-05-15 18:55:11 -0300

    :sparkles: feat: Cria a função deletePost na camada controller

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -37,9 +37,17 @@ const updatePost = async (req, res) => {
   return res.status(200).json(message);
 };
 
+const deletePost = async (req, res) => {
+  const { user: { id: userId }, params: { id } } = req;
+  const { type, message } = await postService.deletePost({ userId, id });
+  if (type) return res.status(type).json({ message });
+  return res.status(204).json({});
+};
+
 module.exports = {
   setPost,
   getAllPost,
   findPostById,
   updatePost,
+  deletePost,
 };
