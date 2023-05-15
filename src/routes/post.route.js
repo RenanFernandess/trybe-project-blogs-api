4769858bfb6884f32a8ef8e75587a56b5c2475da
Renan Fernandes <renzinestuods@gmail.com>
2023-05-15 19:50:34 -0300

    :sparkles: feat: Adiciona a rota GET '/post/search'

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -8,6 +8,8 @@ postRoute.post('/', validateToken, postController.setPost);
 
 postRoute.get('/', validateToken, postController.getAllPost);
 
+postRoute.get('/search', validateToken, postController.findPostByQuery);
+
 postRoute.get('/:id', validateToken, postController.findPostById);
 
 postRoute.put('/:id', validateToken, postController.updatePost);
